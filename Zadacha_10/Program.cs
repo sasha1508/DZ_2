b9962852f85d3e8a857002c98004d6492d7f09d5
Александр <sasha@localhost.localdomain>
2023-02-16 20:18:01 +0300

    Первая задача готова

--- Zadacha_10/Program.cs
+++ Zadacha_10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введи трёхзначное число: ");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+if (Chislo < 100 && Chislo > 999) 
+{
+    Console.Write("Введено не трёхзначное число");
+}
+else
+{
+    int VtorayaCifra = (Chislo / 10) % 10;
+    Console.WriteLine($"Вторая цифра: {VtorayaCifra}");
+}
