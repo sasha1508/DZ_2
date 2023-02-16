93339694f45608bb66e37719a8c5c52981954aac
Александр <sasha@localhost.localdomain>
2023-02-16 21:01:36 +0300

    Вторая задача готова

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного семизначного числа или сообщает, что третьей цифры нет.
+
+// 78 -> третьей цифры нет
+
+// 3267900 -> 6
+
+// ДОП: цифра третья может быть как с левой стороны, так и с правой
+
+Console.Clear();
+Console.Write("Введи семизначное число: ");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+if (Chislo < 1000000 || Chislo > 9999999) 
+{
+    Console.Write("Введено не семизначное число");
+}
+else
+{
+    int VtorayaCifraSleva = (Chislo / 10000) % 10;
+    int VtorayaCifraSprava = (Chislo / 100) % 10;
+    Console.WriteLine($"Третья цифра слева: {VtorayaCifraSleva}");
+    Console.WriteLine($"Третья цифра справа: {VtorayaCifraSprava}");
+}
